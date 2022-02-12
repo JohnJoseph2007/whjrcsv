c2565dcc4697efdbf6e0129a091214ca827f0e5e
John <write2johnjoseph@gmail.com>
2022-02-12 09:34:35 +0530

    project c104

--- Projects/104/PythonFiles/main.py
+++ Projects/104/PythonFiles/main.py
@@ -1,9 +1,29 @@
+from collections import Counter
 import pandas as pd
 
 # Set Up:
 df = pd.read_csv("data.csv")
-data = df['Weight(Pounds)'].tolist()
+datar = df['Weight(Pounds)'].tolist()
+data = datar
+data.sort()
+print(data)
 
 # mean:
 mean = sum(data)/len(data)
 print(mean)
+
+# median:
+middle = len(data)//2
+if len(data)%2==0:
+  median = (data[middle] + data[middle+1])/2
+  print(median)
+elif len(data)%2==1:
+  median = data[middle]
+  print(median)
+
+# mode:
+datacount = Counter(datar)
+a = datacount.values()
+maximum = max(a)
+x = [i for i, frequency in datacount.items() if frequency==maximum]
+print(*(x))
---
+++ Projects/104/README.md
@@ -0,0 +1,2 @@
+# GOOGLE COLAB LINK:
+[C-104 Project](https://colab.research.google.com/drive/1VvJqSwk2B7y7VZZyo49_-5kgF7DwCxVl?usp=sharing)
